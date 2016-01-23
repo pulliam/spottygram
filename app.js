a87f479c941061d3e2aa96a17b5953ea8fa2f08c
Thais Pulliam <thaispulliam@gmail.com>
2016-01-23 18:21:42 -0500

    first setups of nodemailer' '

--- app.js
+++ app.js
@@ -113,29 +113,33 @@ app.get('/contact', function (req, res) {
 
 app.post('/contact', function (req, res) {
   var mailOpts, smtpTrans;
-  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
   smtpTrans = nodemailer.createTransport('SMTP', {
       service: 'Gmail',
       auth: {
-          user: "thaispulliam@gmail.com",
-          pass: "application-specific-password" 
+          user: "spottygram@gmail.com",
+          pass: process.env.spottygrampassword
       }
   });
-  //Mail options
   mailOpts = {
       from: req.body.name + ' &lt;' + req.body.email + '&gt;', //grab form data from the request body object
-      to: 'me@gmail.com',
+      to: 'spottygram@gmail.com',
       subject: 'Website contact form',
       text: req.body.message
   };
   smtpTrans.sendMail(mailOpts, function (error, response) {
       //Email not sent
       if (error) {
-          res.render('contact', { title: 'Raging Flame Laboratory - Contact', msg: 'Error occured, message not sent.', err: true, page: 'contact' })
+          res.render('contact', 
+            { title: 'Raging Flame Laboratory - Contact', 
+            msg: 'Error occured, message not sent.', 
+            err: true, page: 'contact' })
       }
       //Yay!! Email sent
       else {
-          res.render('contact', { title: 'Raging Flame Laboratory - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' })
+          res.render('contact', 
+            { title: 'Raging Flame Laboratory - Contact', 
+            msg: 'Message sent! Thank you.', 
+            err: false, page: 'contact' })
       }
   });
 });
